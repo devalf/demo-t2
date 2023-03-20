a5bb374e020a1b5e06eb1e1e3ba5a387aa9cb7cb
Oleksiy Petrychko <mykolasich@gmail.com>
2023-03-20 22:24:26 +0200

    Initialization App based on Mobx state manager

---
+++ src/client-mx/webpack.config.js
@@ -0,0 +1,116 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const {PORT_CLIENT = 8082, PORT_API_SERVER = 8081} = process.env;
+
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === 'development';
+
+    return ({
+        mode: isDevelopment ? 'development' : 'production',
+        entry: [
+            'react-hot-loader/patch',
+            './src/index.tsx'
+        ],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    use: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ],
+                    exclude: /\.module\.css$/
+                },
+                {
+                    test: /\.ts(x)?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: true
+                            }
+                        }
+                    ],
+                    include: /\.module\.css$/
+                },
+                {
+                    test: /\.svg$/,
+                    use: 'file-loader'
+                },
+                {
+                    test: /\.png$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                mimetype: 'image/png'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname, './dist')
+            },
+            host: '0.0.0.0',
+            port: PORT_CLIENT,
+            hot: true,
+            historyApiFallback: true
+        },
+        plugins: [
+            // new CopyPlugin({
+            //     patterns: [{from: 'index.html'}]
+            // }),
+            new HtmlWebpackPlugin({
+                template: 'index.html'
+            }),
+            new LodashModuleReplacementPlugin,
+            new MiniCssExtractPlugin(),
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                openAnalyzer: false
+            }),
+            new CleanWebpackPlugin()
+        ],
+        resolve: {
+            extensions: [
+                '.tsx',
+                '.ts',
+                '.js'
+            ]
+        },
+        ...(isDevelopment && {devtool: 'eval-source-map'}),
+        ...(!isDevelopment && {
+            performance: {
+                hints: false,
+                maxEntrypointSize: 512000,
+                maxAssetSize: 512000
+            }
+        })
+    });
+};
